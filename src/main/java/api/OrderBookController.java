c20fcf9da9a2c298f7cb4f8bdbe77ef545f8f843
LoganMallory <logan.mallory@gmail.com>
2020-09-28 16:51:04 -0500

    Market orders are now Immediate Or Cancel, don't live in order book

--- src/main/java/api/OrderBookController.java
+++ src/main/java/api/OrderBookController.java
@@ -22,14 +22,14 @@ public class OrderBookController {
 
     @JsonView(Views.OrderBook.class)
     @RequestMapping(method = RequestMethod.GET, value = "/orders")
-    public OrderBook getOrderBok() {
+    public OrderBook getOrderBook() { //view the entire order book (just prices & quantities)
         logger.info("GET /orders");
         return this.orderBook;
     }
 
     @JsonView(Views.Order.class)
     @RequestMapping(method = RequestMethod.GET, value = "/orders/{ids}")
-    public Order[] getOrders(@PathVariable final String[] ids) {
+    public Order[] getOrders(@PathVariable final String[] ids) { //view status of orders (id, side, price, quantity)
         logger.info("GET /orders/{}", Arrays.toString(ids));
         final Order[] orders = new Order[ids.length];
         for(int i=0; i < ids.length; i++) {
@@ -39,7 +39,7 @@ public class OrderBookController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/orders")
-    public Message placeOrder(@RequestBody Order order) {
+    public Message placeOrder(@RequestBody Order order) { //place a new order
         logger.info("POST /orders (order={})", order.toString());
         if(!order.getSymbol().equals(this.orderBook.getSymbol())) return new Message("Order rejected. Invalid symbol (" + order.getSymbol() + ") for order book with symbol " + this.orderBook.getSymbol());
         if(order.getSide() != -1 && order.getSide() != 1) return new Message("Order rejected. Invalid side (" + order.getSide() + "). Side must be -1 or 1");
@@ -49,4 +49,11 @@ public class OrderBookController {
         return new Message("Order accepted. Id: " +  order.getId());
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "/orders/{id}")
+    public Message updateOrder(@PathVariable final String id, @RequestParam final long price, @RequestParam final long quantity) {
+        if(price < 0) return new Message("Order update rejected. Invalid price (" + price + "). Price must be >= 0");
+        if(quantity < 1) return new Message("Order update rejected. Invalid quantity (" + quantity + "). Quantity must be > 0");
+        if(this.orderBook.updateOrder(id, price, quantity) != 0) new Message("Order update rejected. Invalid id (" + id + ")");
+        return new Message("Order " + id + " updated.");
+    }
 }
