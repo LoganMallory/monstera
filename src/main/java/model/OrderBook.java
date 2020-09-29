fd2288abaaa5a1cd68cbcba13b634e6fdd9d6dff
LoganMallory <logan.mallory@gmail.com>
2020-09-28 19:15:40 -0500

    Update last traded price after every order

--- src/main/java/model/OrderBook.java
+++ src/main/java/model/OrderBook.java
@@ -1,19 +1,30 @@
 package model;
 
+import api.OrderBookController;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLong;
 
+//TODO: enforce limits on number of client orders (per client, and total)
 public class OrderBook {
     @JsonView(Views.OrderBook.class)
     private final String symbol;
     private final OrderQueue[] offers;     //we can use bit shifting to calculate index and avoid conditional branching
+    private final int pricePrecision;      //2 = cents, 3 = tenth of a cent
+    private AtomicLong lastTradedPrice;
+    private final Logger logger;
 
     public OrderBook(final String symbol) {
-        this.symbol = symbol;
-        this.offers = new OrderQueue[2];
-        this.offers[0] = new OrderQueue(511); //bids
-        this.offers[1] = new OrderQueue(511); //asks
+        this.symbol          = symbol;
+        this.offers          = new OrderQueue[2];
+        this.offers[0]       = new OrderQueue(4095); //bids
+        this.offers[1]       = new OrderQueue(4095); //asks
+        this.pricePrecision  = 2;
+        this.lastTradedPrice = new AtomicLong(-1);
+        this.logger          = LoggerFactory.getLogger(OrderBookController.class);
     }
 
     public String getSymbol() {
@@ -30,44 +41,38 @@ public class OrderBook {
         return this.offers[1].asArrayCopy();
     }
 
+    @JsonView(Views.OrderBook.class)
+    public long getLastTradedPrice() {
+        return this.lastTradedPrice.get();
+    }
+
     public void processOrder(Order order) {
-        final int idx = order.getSide() >>> 31; //will be 0 if side == 1, or 1 if side == -1
-        final long orderQuantity = order.getRemainingQuantity();
-        final double avgFillPrice = this.offers[idx^1].fillOrder(order);
+        final int idx             = order.getSide() >>> 31; //will be 0 if side == 1, or 1 if side == -1
+        final long orderQuantity  = order.getRemainingQuantity();
+        final double avgFillPrice = this.offers[idx^1].fillOrder(order, this.lastTradedPrice);
         final long quantityFilled = orderQuantity - order.getRemainingQuantity();
         if(quantityFilled > 0) {
-            //TODO: report trade, other stuff
+            logger.info("Order " + order.getId() + " filled: " + avgFillPrice + " x " + quantityFilled);
+            //TODO: report trade (how to get other order's id?), other stuff
         }
-        if(quantityFilled < orderQuantity) {
+        if(quantityFilled < orderQuantity && order.getPrice() > 0) { //order was only partially filled, add remaining to queue (except market orders)
             this.offers[idx].add(order);
-        } //else trade was fully filled
+        } //else order was fully filled, so dont put it in the queue
+    }
+
+    public int updateOrder(final String id, final long price, final long quantity) {
+        final int idx     = ((id.charAt(0) - '0') ^ 1) & 1; //first char in ID is order side (0 for ask, 1 for bid), & 1 keeps idx in bounds
+        final Order order = this.offers[idx].getOrder(id);
+        if(order == null) return -1;
+        this.offers[idx].remove(id); //remove the order first
+        order.setPrice(price);
+        order.setQuantity(quantity);
+        this.processOrder(order);    //match order against opposite side, and place in heap if not fully filled
+        return 0;
     }
 
     public Order getOrder(String id) {
-        final int idx = (id.charAt(0) - '0') ^ 1; //first char in ID is order side (0 for ask, 1 for bid)
-        return this.offers[idx & 1].getOrder(id); //& 1 makes sure idx is never out of bounds (avoids conditional branching too)
-    }
-
-    public String toString() {
-        Order[] bidArray = this.offers[0].asArrayCopy();
-        Order[] askArray = this.offers[1].asArrayCopy();
-        Arrays.sort(bidArray);
-        Arrays.sort(askArray);
-
-        final StringBuilder book = new StringBuilder("Order Book:\n");
-
-        int i=0;
-        int j=0;
-
-        while(i < bidArray.length && j < askArray.length) {
-            book.append("\t").append(bidArray[i++].toString(true)).append("  -  ").append(askArray[j++].toString(false));
-        }
-        while(i < bidArray.length) {
-            book.append("\t").append(bidArray[i++].toString(true)).append("  -  ");
-        }
-        while(j < askArray.length) {
-            book.append("\t\t\t\t\t\t\t\t\t\t-  ").append(askArray[j++].toString(false));
-        }
-        return book.toString();
+        final int idx = ((id.charAt(0) - '0') ^ 1) & 1; //first char in ID is order side (0 for ask, 1 for bid), & 1 keeps idx in bounds
+        return this.offers[idx].getOrder(id);
     }
 }
